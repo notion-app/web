f2b8a1b49b29344f540fedd2a498a5acf35e6e84
Aaron Peters <a.mpeters24@gmail.com>
2015-09-21 19:42:36 -0400

    Added basic layout for notebook view

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Route, DefaultRoute} from 'react-router';
 
 import LandingPageComponent from 'components/landingpage.js';
-import ExampleComponent from 'components/example'
+import ExampleComponent from 'components/example';
+import NotebookView from 'components/notebookview';
 import App from 'components/app'
 
 
@@ -10,6 +11,7 @@ const routes = (
   <Route handler={App} >
     <Route name='Landing Page' path='/' handler={LandingPageComponent}/>
     <Route name='example' path='/example' handler={ExampleComponent}/>
+    <Route name='notebookview' path='/notebookview' handler={NotebookView}/>
   </Route>
 );
 
